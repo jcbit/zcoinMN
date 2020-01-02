5e03285f974630668c024444e7e6f6cd3e21013b
jcbit <58643970+jcbit@users.noreply.github.com>
2020-01-02 16:05:44 -0600

    Update UpgradeMN.sh

--- UpgradeMN.sh
+++ UpgradeMN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt -y autoremove
-wget https://github.com/zcoinofficial/zcoin/releases/download/v0.13.8.9/zcoin-0.13.8.9-linux64.tar.gz
+wget https://github.com/zcoinofficial/zcoin/releases/download/v0.13.8.10/zcoin-0.13.8.10-linux64.tar.gz
 cd ~/zcoin-0.13.8/bin
 ./zcoin-cli stop
 cd ~
